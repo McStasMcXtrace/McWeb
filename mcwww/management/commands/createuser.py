5f85d6bb8717439382a9b28cf688b274b35413ce
jaga-mcstas <jaga@fysik.dtu.dk>
2015-09-14 18:44:04 +0200

    fix of createuser to match django version 1.8

--- mcwww/management/commands/createuser.py
+++ mcwww/management/commands/createuser.py
@@ -1,47 +1,43 @@
 from django.core.management.base import BaseCommand
-
-import sys
+from django.contrib.auth.models import User, Group
 from getpass import getpass
 
-from django.contrib.auth.models import User, Group
-
-def main(args):
-    if len(args) < 1:
-        print 'usage: %s username [password]' % args[0]
-        sys.exit(1)
-
-    username = args[0]
-    print 'Username:', username
-
-    if User.objects.filter(username=username).count() > 0:
-        print 'Abort: User already exists!'
-        sys.exit(1)
-
-    if len(args) > 1:
-        print 'Password taken from argument'
-        password = args[1]
-    else:
-        password = getpass('Enter password: ')
-    
-    user = User.objects.create_user(username, password=password)
-    user.save()
-    
-    if len(args) > 2:
-        for idx in range(2,len(args)):
-            print '- adding user to group '+args[idx]
-            try:
-                g = Group.objects.get(name=args[idx]) 
-                g.user_set.add(user)
-            except:
-                print 'Error: Group '+args[idx]+' does not exist!'
-
-    print 'User created.'
+import sys
 
 class Command(BaseCommand):
-    username = 'username'
-    password = 'password'
-
-    help = "Whatever you want to print here"
+    def add_arguments(self, parser):
+        parser.add_argument('username', nargs=1, type=str)
+        parser.add_argument('password', nargs=1, type=str)
+        parser.add_argument('group', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        main(args)
+        if len(options) < 1:
+            print 'usage: %s username [password]' % options[0]
+            sys.exit(1)
+    
+        username = options['username']
+        print 'Username:', username
+    
+        if User.objects.filter(username=username).count() > 0:
+            print 'Abort: User already exists!'
+            sys.exit(1)
+    
+        if len(options) > 1:
+            print 'Password taken from argument'
+            password = options['password']
+        else:
+            password = getpass('Enter password: ')
+        
+        user = User.objects.create_user(username, password=password)
+        user.save()
+        
+        if len(options) > 2:
+            for idx in range(0,len(options['group'])):
+                print '- adding user to group '+options['group'][idx]
+                try:
+                    g = Group.objects.get(name=options['group'][idx]) 
+                    g.user_set.add(user)
+                except:
+                    print 'Error: Group '+options['group'][idx]+' does not exist!'
+    
+        print 'User created.'
