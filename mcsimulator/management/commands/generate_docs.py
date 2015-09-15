0f80c4b101ace699252cd58de8e6c9a9a642904e
jaga-mcstas <jaga@fysik.dtu.dk>
2015-09-15 14:31:53 +0200

    fix of generate_docs command for Django 1.8

--- mcsimulator/management/commands/generate_docs.py
+++ mcsimulator/management/commands/generate_docs.py
@@ -1,33 +1,16 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 
-from optparse import make_option
 import glob, re
 
-
-def grab_list():
-    return glob.glob('sim/*.instr')
-
-
-def gen_html(instr):
-    src = file(instr).read()
-    doc = re.search(r'^/\*\*+(.*)\%[Ee]', src,
-                    re.MULTILINE + re.DOTALL).group(1)
-
-    out = instr + '.doc.txt'
-    file(out, 'w').write(doc)
-
-
-def main():
-    map(gen_html, grab_list())
-
-
-class Command(NoArgsCommand):
-
-    help = "Whatever you want to print here"
-
-    option_list = NoArgsCommand.option_list + (
-        make_option('--verbose', action='store_true'),
-    )
-
-    def handle_noargs(self, **options):
-        main()
+class Command(BaseCommand):
+    
+    def handle(self, *args, **options):
+        grabbed = glob.glob('sim/*.instr')
+        
+        for instr in grabbed:
+            src = file(instr).read()
+            doc = re.search(r'^/\*\*+(.*)\%[Ee]', src, re.MULTILINE + re.DOTALL).group(1)
+            out = instr + '.doc.txt'
+            print('Writing file... %s' % out)
+            file(out, 'w').write(doc)
+        
