e5232396a192abcbcf6656903032e4bbfdee1fad
jaga-mcstas <jaga@fysik.dtu.dk>
2015-09-15 17:27:20 +0200

    fixed generate_docs

--- mcsimulator/management/commands/generate_docs.py
+++ mcsimulator/management/commands/generate_docs.py
@@ -1,13 +1,20 @@
 from django.core.management.base import BaseCommand
+from mcwww import settings
 
-import glob, re
+import re
+import os
 
 class Command(BaseCommand):
     
     def handle(self, *args, **options):
-        grabbed = glob.glob('sim/*.instr')
         
-        for instr in grabbed:
+        instr_files = []
+        for dirpath, _dirnames, filenames in os.walk(os.path.join(settings.BASE_DIR, 'sim')):
+            for f in filenames:
+                if re.search('.instr', f):
+                    instr_files.append(os.path.join(dirpath, f))
+        
+        for instr in instr_files:
             src = file(instr).read()
             doc = re.search(r'^/\*\*+(.*)\%[Ee]', src, re.MULTILINE + re.DOTALL).group(1)
             out = instr + '.doc.txt'
