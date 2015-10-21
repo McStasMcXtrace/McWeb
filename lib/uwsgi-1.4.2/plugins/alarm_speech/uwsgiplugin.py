0d4ba5baf80bc1434859a07aceb071cf398772a2
Jakob Garde <jaga@mcstas-01.risoe.dk>
2015-10-21 15:04:12 +0200

    removed local uwsgi (now getting it through pip)

--- lib/uwsgi-1.4.2/plugins/alarm_speech/uwsgiplugin.py
+++
@@ -1,6 +0,0 @@
-NAME='alarm_speech'
-
-CFLAGS = []
-LDFLAGS = []
-LIBS = ['-framework appkit']
-GCC_LIST = ['alarm_speech.m']
--- lib/uwsgi-1.4.2/plugins/emperor_mongodb/uwsgiplugin.py
+++
@@ -1,7 +0,0 @@
-NAME='emperor_mongodb'
-
-CFLAGS = ['-I/usr/include/mongo','-I/usr/local/include/mongo']
-LDFLAGS = []
-LIBS = ['-lmongoclient', '-lboost_thread','-lboost_filesystem']
-
-GCC_LIST = ['plugin', 'emperor_mongodb.cc']
