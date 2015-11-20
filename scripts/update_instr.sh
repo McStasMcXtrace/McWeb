9ad110e1aa591360e48bf876bdfd09216304d3fd
jaga-mcstas <jaga@fysik.dtu.dk>
2015-11-20 15:13:19 +0100

    update-instruments script for cron use. The script looks for sim folder checksum changes, but does not back up the django db.

---
+++ scripts/update_instr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+OLD=$(cat checksum)
+
+find sim/ -type f -exec md5sum {} \; | sort -k 34 | md5sum | cut -d' ' -f1 > newchecksum
+NEW=$(cat newchecksum)
+rm newchecksum
+
+if [ $OLD == $NEW ]; then
+	echo sim folder checksum has not changed, exiting.
+	echo
+	exit
+elif [ $OLD != $NEW ]; then
+	echo sim folder checksum has changed, running updatesimulations.sh ...
+	echo
+	bin/update-simulations.sh
+	echo updating django instrument db ...
+	echo
+	python manage.py populate_db
+	echo creating new checksum file...
+	rm checksum
+	find sim/ -type f -exec md5sum {} \; | sort -k 34 | md5sum | cut -d' ' -f1 > checksum
+	cat checksum
+fi
+
