1c47bb8ffe32a89d7dbd3f5b03317ae6d3d87311
www-data <www-data@mcstas-01.risoe.dk>
2015-11-25 16:07:41 +0100

    new default for LDAP dn (risoe.dk)

--- mcwww/settings.py
+++ mcwww/settings.py
@@ -5,7 +5,7 @@ from django_auth_ldap.config import LDAPSearch
 AUTH_LDAP_SERVER_URI = "ldap://localhost"
 AUTH_LDAP_BIND_DN = ""
 AUTH_LDAP_BIND_PASSWORD = ""
-AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=users,dc=fysik,dc=dtu,dc=dk", ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
+AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=users,dc=risoe,dc=dk", ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
 AUTHENTICATION_BACKENDS = (
     'django_auth_ldap.backend.LDAPBackend',
     #'django.contrib.auth.backends.ModelBackend', # uncomment this line to enable local sign-on (django-db)
