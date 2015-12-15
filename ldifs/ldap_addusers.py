ee12e5f8ab47da2b72d6c23d1a0761b0ce244dd5
jaga-mcstas <jaga@fysik.dtu.dk>
2015-12-15 20:03:01 +0100

    improved ldap addusers script by handing uid_number better, applying firstname and lastname, and using the new csv format (also omitting the first line)

--- ldifs/ldap_addusers.py
+++ ldifs/ldap_addusers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-Init the mcldap db by running this script. Then users may be added.
+This scripts adds LDAP users. Init the mcldap db before running this script.
 '''
 import logging
 import argparse
@@ -9,15 +9,25 @@ import subprocess
 import os
 import csv
     
-def ldap_adduser(dn, admin_password, uid, user_password='hest', uid_number=1001):
-    with open ("cn_user.ldif", "r") as ldif_template:
+def ldap_adduser(dn, admin_password, cn, sn, uid, pw='hest', uid_number=1001):
+    ''' 
+    cn: firstname
+    sn: lastname
+    uid: username
+    pw: password
+    
+    uid_number: a unique number for this user in the LDAP db (must be kept track of)
+    '''
+    with open ('cn_user.ldif', 'r') as ldif_template:
         cn_user=ldif_template.read()
         ldif_template.close()
     
     cn_user = cn_user.replace('DN', dn)
+    cn_user = cn_user.replace('UCN', cn)
+    cn_user = cn_user.replace('USN', sn)
     cn_user = cn_user.replace('UID', uid)
     cn_user = cn_user.replace('uID_NUM', str(uid_number))
-    cn_user = cn_user.replace('PASSWORD', user_password)    
+    cn_user = cn_user.replace('PASSWORD', pw)    
     
     ldif = open('_cn_user.ldif', 'w')
     ldif.write(cn_user)
@@ -28,19 +38,50 @@ def ldap_adduser(dn, admin_password, uid, user_password='hest', uid_number=1001)
     finally:
         os.remove('_cn_user.ldif')
 
+def get_dn():
+    process = subprocess.Popen('sudo slapcat | head -n1 | cut -d" " -f2',
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               shell=True)
+    (stdout, stderr) = process.communicate()
+    return stdout.rstrip('\n')
+
+def get_new_uid():
+    ''' opens uidindexfile, iterates, saves and returns '''
+    if not os.path.exists('uidindexfile'):
+        with open('uidindexfile', 'w') as uidif:
+            uidif.write('10001')
+    
+    current = -1
+    with open('uidindexfile', 'r') as uidif:
+        current = uidif.read().strip()
+        uidif.close()
+    with open('uidindexfile', 'w') as uidif:
+        next = int(current) + 1
+        uidif.write(str(next))
+        uidif.close()
+        return next
+
 def main(args):
     logging.basicConfig(level=logging.INFO)
-
-    # NOTE: use 'sudo slapcat | head -n1 | cut -d" " -f2' to get the dn
     
+    dn = get_dn()
+    
+    #debug = True
+    #if debug:
+    #    dn = 'dc=fysik,dc=dtu,dc=dk'
+        
     with open(args.users_csv[0], 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
+        reader = csv.reader(csvfile, delimiter=';')
+        firstline = True
         for row in reader:
-            ldap_adduser(args.dn[0], args.password[0], row[0], row[1])
+            if not firstline:
+                nextuid = get_new_uid()
+                ldap_adduser(dn, args.password[0], cn=row[0], sn=row[1], uid=row[2], uid_number=nextuid)
+            firstline = False
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('dn', nargs='+', help='run slapcat to identify (e.g. dc=fysik,dc=dtu,dc=dk)')
     parser.add_argument('password', nargs='+', help='ldap admin password')
     parser.add_argument('users_csv', nargs='+', help='csv file containing user data')
     args = parser.parse_args()
