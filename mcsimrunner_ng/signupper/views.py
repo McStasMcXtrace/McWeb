0c7491568330be5b5476f020e14c6c08ca78cc8d
jaga-mcstas <jaga@fysik.dtu.dk>
2015-12-16 11:51:14 +0100

    implemented signup funcionality using dynamic couse list; signup form generation, and random passwords generated for the new_signups.csv lines

--- mcsimrunner_ng/signupper/views.py
+++ mcsimrunner_ng/signupper/views.py
@@ -4,30 +4,65 @@ the thank you page.
 '''
 from django.shortcuts import render, redirect
 from os.path import exists
+from subprocess import Popen, PIPE
+from mcweb import settings
 
 def signup(req):
     ''' displays the signup form '''
-    return render(req, 'signup.html')
+    return render(req, 'signup.html', {'courses': settings.COURSES})
+
+def get_random_passwd():
+    try:
+        process = Popen('makepasswd', stdout=PIPE, stderr=PIPE, shell=True)
+        (stdoutdata, stderrdata) = process.communicate()
+        return stdoutdata.strip()
+    except:
+        print 'get_random_passwd() --> Popen("makepasswd") output:\n' + stderrdata
+        raise
+
+def cols_to_line(cols):
+    ''' makes a list of strings into a single ;-separated string with a trailing linebreak '''
+    line = ""
+    for c in cols:
+        line = "%s%s;" % (line, c)
+    return line.rstrip(";") + "\n"
 
 def signup_get(req):
-    ''' signup form GET parse and save signup to file '''
+    ''' signup form GET parsing, and append the signup line to file new_signups.csv '''
+
+    # introduce the file
+    csv = 'new_signups.csv'
+    
+    # and the form
     form = req.GET
+    
+    # get static fields from the form
     firstname = form.get('firstname')
     lastname = form.get('lastname')
     username = form.get('username')
     email = form.get('email')
-    password = ""
+    password = get_random_passwd()
+    cols = [firstname, lastname, username, email, password]
     
-    csv = 'new_signups.csv'
-    header_str = ""
+    # get dynamic fields from the form
+    for c in settings.COURSES:
+        cols.append(str(bool(form.get(c))))
+    
+    # create the header line - an empty string if the file exists
+    header_line = ""
     if not exists(csv):
-        header_str = "firstname;lastname;username;email;password\n"
+        header_cols = ["firstname", "lastname", "username", "email", "password"]
+        for c in settings.COURSES:
+                header_cols.append(c)
+        header_line = cols_to_line(header_cols)
     
+    # write to file
     with open(csv, 'a') as f:
-        f.write(header_str)
-        f.write('%s;%s;%s;%s;%s\n' % (firstname, lastname, username, email, password))
+        f.write(header_line)
+        f.write(cols_to_line(cols))
         f.close()
     
+    # get a thank-you message to the user
     return redirect('/thanks/')
 
 def thanks(req):
