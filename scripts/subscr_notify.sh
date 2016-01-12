e01e1b8ab7c70e32960ac34431104c73681b2d8d
Peter Willendrup <pkwi@fysik.dtu.dk>
2016-01-12 12:40:36 +0100

    cron scripts fixed and updated

--- scripts/subscr_notify.sh
+++ scripts/subscr_notify.sh
@@ -1,1 +1,2 @@
-
+#!/bin/bash
+python notify_users.py $*
--- scripts/subscr_tasks.sh
+++ scripts/subscr_tasks.sh
@@ -1,5 +1,11 @@
-YEARDATE="$(python gettodaystring.py)"
-echo "moving ../mcsimrunner_ng/new_signups.csv to ../useradmin/new_${YEARDATE}.csv ..."
-mv ../mcsimrunner_ng/new_signups.csv ../useradmin/new_"${YEARDATE}".csv
-echo "sending email to admin with instructions ..."
-mailx -s "mcweb subscriptions: new_${YEARDATE}.csv" jaga@fysik.dtu.dk < subscr_email
+#!/bin/bash
+# this script should be run once every night to send a notification to the admin in there are new signups
+if [ -f /srv/mcweb/McWeb/mcsimrunner_ng/new_signups.csv ]; then
+    YEARDATE="$(python /srv/mcweb/McWeb/scripts/gettodaystring.py)"
+    echo "moving new_signups.csv to /srv/mcweb/McWeb/useradmin/new_${YEARDATE}.csv ..."
+    mv /srv/mcweb/McWeb/mcsimrunner_ng/new_signups.csv /srv/mcweb/McWeb/useradmin/new_"${YEARDATE}".csv
+    echo "sending email to admin with instructions ..."
+    mailx -s "mcweb subscriptions: new_${YEARDATE}.csv" jaga@fysik.dtu.dk < /srv/mcweb/McWeb/scripts/subscr_email
+else
+    echo "no new signups, exiting (file new_signups.csv does not exist)"
+fi
