a82d2cd4f5637e2ec9a2602a8c485bbed095c639
Peter Willendrup <pkwi@fysik.dtu.dk>
2016-01-12 12:47:56 +0100

    renamed a few script files to communicate what is a script and what is an extra file

--- scripts/subscr_tasks.sh
+++ scripts/subscr_tasks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # this script should be run once every night to send a notification to the admin in there are new signups
 if [ -f /srv/mcweb/McWeb/mcsimrunner_ng/new_signups.csv ]; then
-    YEARDATE="$(python /srv/mcweb/McWeb/scripts/gettodaystring.py)"
+    YEARDATE="$(python /srv/mcweb/McWeb/scripts/_gettodaystring.py)"
     echo "moving new_signups.csv to /srv/mcweb/McWeb/useradmin/new_${YEARDATE}.csv ..."
     mv /srv/mcweb/McWeb/mcsimrunner_ng/new_signups.csv /srv/mcweb/McWeb/useradmin/new_"${YEARDATE}".csv
     echo "sending email to admin with instructions ..."
-    mailx -s "mcweb subscriptions: new_${YEARDATE}.csv" jaga@fysik.dtu.dk < /srv/mcweb/McWeb/scripts/subscr_email
+    mailx -s "mcweb subscriptions: new_${YEARDATE}.csv" jaga@fysik.dtu.dk < /srv/mcweb/McWeb/scripts/_adminemail
 else
     echo "no new signups, exiting (file new_signups.csv does not exist)"
 fi
