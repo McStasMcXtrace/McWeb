66bbd0a8fe3562fd3a95416f1b2bd144c55e44cb
jaga-mcstas <jaga@fysik.dtu.dk>
2016-01-13 16:44:58 +0100

    uploader: polishing, now users have to actively select a group to upload, and improved messages

--- mcsimrunner_ng/uploader/views.py
+++ mcsimrunner_ng/uploader/views.py
@@ -5,6 +5,9 @@ from mcweb.settings import BASE_DIR, FILE_UPLOAD_PW
 def group_dirs():
     ''' lists base level file directories in sim/ '''
     for _, dirs, _ in os.walk('sim/'):
+        # stackoverflow-ish alphanum sort 
+        dirs = sorted(dirs, key=lambda item: (int(item.partition(' ')[0])
+                               if item[0].isdigit() else float('inf'), item))
         return dirs
 
 def shepherd(file_obj, g):
@@ -17,17 +20,20 @@ def shepherd(file_obj, g):
 
 def upload(request):
     ''' handles upload post and injects another form '''
+    groupdirs = group_dirs()
+    message = ''
+    
+    # try to get form
     form = request.POST
     if not form:
-        return render(request, 'uploader.html', {'groupdirs': group_dirs(), 'password': '', 'message': ''})
+        return render(request, 'uploader.html', {'groupdirs': groupdirs, 'password': '', 'message': ''})
     
     # check password
     pw = form['password']
     if pw == '':
-        return render(request, 'uploader.html', {'groupdirs': group_dirs(), 'password': '', 'message': ''})
-    
+        return render(request, 'uploader.html', {'groupdirs': groupdirs, 'password': '', 'message': ''})
     if pw != FILE_UPLOAD_PW:
-        return render(request, 'uploader.html', {'groupdirs': group_dirs(), 'password': '', 'message': 'Please enter the correct password'})
+        return render(request, 'uploader.html', {'groupdirs': groupdirs, 'password': '', 'message': 'Please enter the correct password'})
     
     # handle form
     g = form['groupdir']
@@ -36,14 +42,19 @@ def upload(request):
     d = form['new_group']
     if d != '':
         os.mkdir('sim/%s' % d)
+        message = 'Group dir %s was created.' % d
+        groupdirs = group_dirs()
         g = d
-        
+    elif g == "select_a_group":
+        return render(request, 'uploader.html', {'groupdirs': groupdirs, 'password': pw, 'message': 'Please select or create a group dir.'})
+    
     # get file
-    message = ''
     if len(request.FILES) > 0:
         f = request.FILES['up_file']
         shepherd(f, g)
         message = 'File %s uploaded to %s.' % (f.name, g)
+    else:
+        return render(request, 'uploader.html', {'groupdirs': groupdirs, 'password': pw, 'message': '%s Please select a file for upload.' % message})
     
     # return render based on current state
-    return render(request, 'uploader.html', {'groupdirs': group_dirs(), 'password': pw, 'message': message})
+    return render(request, 'uploader.html', {'groupdirs': groupdirs, 'password': pw, 'message': message})
