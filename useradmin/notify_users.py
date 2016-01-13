5720eb241a0beb5ccfc27e6cc8f23f63104437ac
jaga-mcstas <jaga@fysik.dtu.dk>
2016-01-13 15:53:14 +0100

    notify_users.py: now edits incoming csv and changes the 'description' valu from 0 to a message string. Implemented handling of various error cases s.a. not to modify csv if no emails were sent, etc.

--- useradmin/notify_users.py
+++ useradmin/notify_users.py
@@ -10,6 +10,7 @@ from datetime import datetime
 import subprocess
 import csv
 import os
+import posix
 
 def printtodaystr():
     ''' prints the todaystring '''
@@ -42,26 +43,63 @@ The e-neutrons.org admin team
         f.close()
         cmd = 'mailx -s "welcome to mcweb" %s < _body' % email
         #print(cmd)
-        subprocess.call(cmd, shell=True)
+        retcode = subprocess.call(cmd, shell=True)
+        if retcode != -1:
+            raise Exception('retcode: %s' % retcode) 
     finally:
         os.remove('_body')
 
+def cols_to_line(cols, delimiter = ','):
+    ''' makes a list of strings into a single ;-separated string with a trailing linebreak '''
+    line = ""
+    for c in cols:
+        line = "%s%s%s" % (line, c, delimiter)
+    return line.rstrip("%s" % delimiter) + "\n"
+
 def main(args):
+    ''' script-specific functionality: with input_file, read lines, send emails and record this in "description" column (index 5) '''
     logging.basicConfig(level=logging.INFO)
     
+    # used to record org and modified lines to be written to csv below
+    text = ''
+
     input_filename = args.notify_users_csv[0]
+    
+    # writable-check to avoid sending emails without being able to record this in the csv
+    if not os.access(input_filename, posix.W_OK):
+        print('refuse, exiting: file %s must be writable' % input_filename)
+        exit()
+    
+    # send notification emails and record all (modified) lines
     with open(input_filename, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-
+        
         for row in reader:
+            # safety for empty or nonsensical lines - skip 
+            if len(row) < 6:
+                continue;
+            
             # filter header line out
             if row[3] != 'email':
-                try:
-                    sendnotify('%s %s' % (row[0], row[1]), row[2], row[3], row[4])
-                    print('notification email sent to %s' % row[3])
-                except Exception as e: 
-                    print('error sending email to %s (%s)' % (row[3], e.__str__()))
+                if row[5] == '0':
+                    try:
+                        sendnotify('%s %s' % (row[0], row[1]), row[2], row[3], row[4])
+                        row[5] = 'user notified by email on %s' % gettodaystr()
+                        
+                        print('notification email sent to %s' % row[3])
+                    except Exception as e: 
+                        print('error sending email to %s (%s)' % (row[3], e.__str__()))
             
+            # record each (un/modified) row as a line of text
+            text = text + cols_to_line(row)
+
+        csvfile.close()
+    
+    # write modified csv to input file
+    with open(input_filename, 'w') as csvfile:
+        csvfile.write(text)
+        csvfile.close()
+        
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('notify_users_csv', nargs=1, help='csv file containing user data')
