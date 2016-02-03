45e0d5a9dbd7d7929245d03e0fc1d41b71fc5a32
jaga-mcstas <jaga@fysik.dtu.dk>
2016-02-03 11:51:12 +0100

    Copied ldap_initdb function from useradmin to signupper and removed ldif file dependence. Added a Django command ldap_initdb for easy access to this function.

--- mcsimrunner_ng/signupper/ldaputils/ldaputils.py
+++ mcsimrunner_ng/signupper/ldaputils/ldaputils.py
@@ -1,6 +1,9 @@
 '''
 ldap functions using ldifs tooo add/remove users, change password and init the db for mcweb usage.
 '''
+import os
+import subprocess 
+
 
 def ldap_adduser():
     pass
@@ -10,6 +13,27 @@ def ldap_rmuser():
 
 def ldap_chpassword():
     pass
+    
+def ldap_initdb(dn, password):
+    ''' inits  the ldap db for mcweb user addition '''
+    # add cn_usergroup
+    cn_usergroup = 'dn: cn=usergroup,%s\ngidNumber: 500\ncn: usergroup\nobjectClass: posixGroup\nobjectClass: top' % dn    
+    ldif = open('_cn_usergroup.ldif', 'a')
+    ldif.write(cn_usergroup)
+    ldif.close()
+    try:
+        process = subprocess.Popen(['ldapadd', '-x', '-w', password, '-D', 'cn=admin,' + dn, '-f', '_cn_usergroup.ldif'])
+        process.communicate()
+    finally:
+        os.remove('_cn_usergroup.ldif')
 
-def ldap_initdb():
-    pass
+    # add ou_users
+    ou_users = 'dn: ou=users,%s\nobjectClass: organizationalUnit\nobjectClass: top\nou: users\n' % dn
+    ldif = open('_ou_users.ldif', 'a')
+    ldif.write(ou_users)
+    ldif.close()
+    try:
+        process = subprocess.Popen(['sudo', 'ldapadd', '-x', '-w', password, '-D', 'cn=admin,' + dn, '-f', '_ou_users.ldif'])
+        process.communicate()
+    finally:
+        os.remove('_ou_users.ldif')
