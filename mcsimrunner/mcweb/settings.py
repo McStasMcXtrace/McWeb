06759e0b642dbfbf43895397e65f93de13ec9b36
jaga-mcstas <jaga@fysik.dtu.dk>
2017-05-31 14:58:57 +0200

    revert to previous debug setup for serving static files

--- mcsimrunner/mcweb/settings.py
+++ mcsimrunner/mcweb/settings.py
@@ -164,8 +164,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, "simrunner/static"),
-#    os.path.join(BASE_DIR, "simrunner/static/logos"),
-#)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
