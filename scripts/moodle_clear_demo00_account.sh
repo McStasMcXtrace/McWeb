32348407886eb82fb98158a777d10b1657c2a4a8
www-data <www-data@mcstas-01.risoe.dk>
2017-06-19 11:08:28 +0200

    Fixes

--- scripts/moodle_clear_demo00_account.sh
+++ scripts/moodle_clear_demo00_account.sh
@@ -1,3 +1,4 @@
 cd /srv/mcweb/moodle
 moosh user-delete demo00
+moosh user-create --auth=ldap --firstname=Demo --lastname=Account --city=Lyngby --country=DK --email=demo00@e-neutrons.org --password=NONE demo00
 moosh course-enrol -r student -s intro-ns demo00
