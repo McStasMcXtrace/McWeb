e84a4e43a90bcb0999b9cd8c3239235332ef8b49
Peter Willendrup <pkwi@fysik.dtu.dk>
2017-08-14 15:33:31 +0200

    Various changes for a Debian 9 / php7 setup

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+STARTDIR=`pwd`
 # Basic infrastructure
 apt-get install net-tools sudo openssh-server xbase-clients
 
@@ -12,13 +13,13 @@ cd /etc/apt/sources.list.d/
 rm mccode.list
 wget http://packages.mccode.org/debian/mccode.list
 apt-get update
-cd -
+cd $STARTDIR
 
 # Base packages for McCode + MPI
 apt-get install mcstas-suite-perl mcstas-suite-python openmpi-bin libopenmpi-dev
 
 # Packages for bootstrapping an McWeb instance
-apt-get install git libsasl2-dev python-dev libldap2-dev libssl-dev python-virtualenv makepasswd
+apt-get install git libsasl2-dev python-dev libldap2-dev libssl-dev python-virtualenv makepasswd nginx mysql-server php-fpm php-mysql
 
 # Install nginx and remove stop apache2 from being default webserver
 update-rc.d apache2 remove
@@ -46,7 +47,24 @@ echo echo Please run the following commands as root to set up your Django with L
 echo echo \\\> python ldap_initdb.py USE_YOUR_LDAP_PASSWD_HERE >>  McWeb_finishup
 echo echo >>  McWeb_finishup
 echo echo Afterwards, please sudo /etc/init.d/uwsgi_mcweb start >>  McWeb_finishup
-cd -
+
+# Moodle
+sudo -u www-data git clone https://github.com/moodle/moodle.git
+echo cd moodle >> moodle_finishup
+echo git checkout MOODLE_33_STABLE >> moodle_finishup
+
+# Moosh
+sudo -u www-data git clone git://github.com/tmuras/moosh.git
+cd moosh
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+php composer.phar install
+ln -sf $PWD/moosh.php /usr/local/bin/moosh
+
+cd $STARTDIR
+
 
 ln -sf /srv/mcweb/McWeb/scripts/uwsgi_mcweb /etc/init.d/uwsgi_mcweb
 update-rc.d uwsgi_mcweb defaults
