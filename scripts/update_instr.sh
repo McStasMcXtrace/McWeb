ca12286d98e5d5493cd50910664e4369f52c59fa
www-data <www-data@elearn1.fysik.dtu.dk>
2017-08-24 15:44:15 +0200

    changed output to only override static/compile_status.html when checksum has changed

--- scripts/update_instr.sh
+++ scripts/update_instr.sh
@@ -14,9 +14,9 @@ if [ "$OLD" == $NEW ]; then
 	echo
 	exit
 elif [ "$OLD" != $NEW ]; then
-	echo sim folder checksum has changed, running collectinstr ...
+	echo sim folder checksum has changed, running collectinstr (output sent to url /static/compile_status.html) ...
 	echo
-	python manage.py collect_instr
+	python manage.py collect_instr > /srv/mcweb/McWeb/mcsimrunner/static/compile_status.html
 	echo creating new checksum file...
 	rm checksum
 	find sim/ -type f -exec md5sum {} \; | sort -k 34 | md5sum | cut -d' ' -f1 > checksum
