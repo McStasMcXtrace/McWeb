3d8dd08354d0f9bd9159ed04a93717879f9c16fa
Peter Willendrup <pkwi@fysik.dtu.dk>
2017-09-06 14:57:42 +0200

    Make a prettier status page

--- scripts/moodle_pullfromgit.sh
+++ scripts/moodle_pullfromgit.sh
@@ -3,8 +3,18 @@
 # Script to "auto-update" moodle installations against whatever latest (chosen) release Moodle
 
 cd /srv/mcweb/moodle
+
+echo "<html>"
+echo "<head>"
+echo "<title>Moodle update status page</title>"
+echo "</head>"
+echo "<body>"
+echo "<h1>Moodle update process:</h1>"
 git checkout `cat ../McWeb/scripts/MOODLE_PREFERRED_VERSION `
 git pull
-
+git status
+echo "<h1>Moosh update process:</h1>"
 cd /srv/mcweb/moosh
 git pull
+git status
+echo "</body>"
