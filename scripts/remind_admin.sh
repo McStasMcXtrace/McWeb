8dacac5a1db6eb9456d8f8a405ee7fd4fb6d8e64
Peter Willendrup <pkwi@fysik.dtu.dk>
2020-03-25 07:39:33 +0100

    Add relevant system paths to let mailserver complete mail delivery

--- scripts/remind_admin.sh
+++ scripts/remind_admin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /srv/mcweb/mcvenv/bin/activate
 cd /srv/mcweb/McWeb/mcsimrunner/
+export PATH=/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/puppetlabs/bin:$PATH
 python manage.py remind_admin
